c69a6a57389790d8a6d549c3453bdef13bb9d8c5
TranAnhTu17 <20020495@vnu.edu.vn>
2022-02-19 17:58:44 +0700

    add slider js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,38 @@
-const menuToggle = document.querySelector('.header-menu-toggle')
-const menu = document.querySelector('.header-menu')
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const menuToggle = $('.header-menu-toggle')
+const menu = $('.header-menu')
+const prevBtn = $('.prev-btn')
+const nextBtn = $('.next-btn')
+const sliderList = $('.testimonial-list')
+const sliderItems = $$('.testimonial-item')
+const sliderLength = sliderItems.length
 
 menuToggle.addEventListener('click', () => {
     menu.classList.add('active')
 })
-
 document.addEventListener('click', (e) => {
     if(!menu.contains(e.target) && !e.target.matches('.header-menu-toggle')) {
         menu.classList.remove('active')
     }
 })
+
+// handle slider
+let index = 0;
+prevBtn.addEventListener('click', () => {
+    handleChangeSlider(-1)
+})
+nextBtn.addEventListener('click', () => {
+    handleChangeSlider(1)
+})
+
+function handleChangeSlider(status) {
+    if(status === -1 && index > 0) {
+        index--
+    } else if(status === 1 && index < sliderLength - 1) {
+        index++
+    }
+    let positionX = -1 * index * sliderList.offsetWidth 
+    sliderList.style = `transform: translateX(${positionX}px)`
+}
